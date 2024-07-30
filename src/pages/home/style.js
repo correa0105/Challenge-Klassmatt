bd5f07922708c2611d2026eedf3ca3798f2c21ca
Lucas Corrêa <correa.l@icloud.com>
2024-07-30 20:05:00 -0300

    Ajuste no background

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -4,5 +4,8 @@ export const HomeContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 100vh;
+    min-height: 100vh;
+
+    font-family: "Inter", sans-serif;
+    background: linear-gradient(340deg, rgba(103,228,255,1) 28%, rgba(12,127,138,1) 100%);
 `;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,11 +4,13 @@ export default createGlobalStyle`
     * {
       margin: 0;
       padding: 0;
+      box-sizing: border-box;
       font-size: 62.5%;
     }
 
-    body {
+    .jogo {
       font-family: "Inter", sans-serif;
       background: linear-gradient(340deg, rgba(103,228,255,1) 28%, rgba(12,127,138,1) 100%);
+      min-height: 100%;
     }
 `;
